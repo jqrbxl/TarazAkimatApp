db5f16c521025e15f60d5535dc5773cdf61498cd
Jqrbxl <zharbol200503@icloud.com>
2025-11-22 17:53:30 +0500

    Initial Commit

---
+++ Taraz AkimatTests/Taraz_AkimatTests.swift
@@ -0,0 +1,17 @@
+//
+//  Taraz_AkimatTests.swift
+//  Taraz AkimatTests
+//
+//  Created by Жарбол Ермаганбет on 22.11.2025.
+//
+
+import Testing
+@testable import Taraz_Akimat
+
+struct Taraz_AkimatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
